00a05e6b049c0b924799132a01b1d50472a7d759
Jose Diaz <Diaz.M.Jose68@gmail.com>
2021-10-08 10:49:10 -0500

    completed the js with html lec

---
+++ js/js-with-html-lecture.js
@@ -0,0 +1,14 @@
+"use strict"; //makes syntax more strict, showing the errors normal js would allow
+
+// alert("TGIF!"); pop up message in web page
+var returnOFAlert = alert("TGIF");
+console.log(returnOFAlert);
+
+//confirm("Any plans for the weekend"); string literal placed into confirm
+var returnOfConfirm = confirm("Any plans for the weekend?");
+console.log(returnOfConfirm); //adding to variable allows to hold on to value
+
+//prompt(message: "what's your fave nu-metal"); asks question
+var favoriteNuMetalBand = prompt("what is your fave nu-metal band");
+console.log(favoriteNuMetalBand);
+
