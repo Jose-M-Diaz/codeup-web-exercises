24b88ac6e3c6a5ada4c46836d7aad4d634157735
Jose Diaz <Diaz.M.Jose68@gmail.com>
2021-11-19 16:26:28 -0600

    completed exercise for jquery

--- js/JQuery_exercise.js
+++ js/JQuery_exercise.js
@@ -1,10 +1,10 @@
 'use strict'
 
-    $('.codeup').css('border', '2px solid black');
-
-var contents = $('#skills').html();
-alert(contents);
-
-$('li').css('font-size', '20px');
-
-$('h1, p, li').css('background-color', '#FFFF00');
+//     $('.codeup').css('border', '2px solid black');
+//
+// var contents = $('#skills').html();
+// alert(contents);
+//
+// $('li').css('font-size', '20px');
+//
+// $('h1, p, li').css('background-color', '#FFFF00');
