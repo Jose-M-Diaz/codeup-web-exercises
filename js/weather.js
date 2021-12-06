9cb65422fd2b01c60a8a94a2dd28546e087e1c97
Jose Diaz <Diaz.M.Jose68@gmail.com>
2021-12-06 15:53:36 -0600

    added for loop to assist with 5day forecast. appended items to div in html

--- js/weather.js
+++ js/weather.js
@@ -26,18 +26,36 @@ $(document).ready(function(){
         }).done(function (data) {
             //console.log('5 day forecast', data);
             console.log(data);
-            date.innerHTML = (new Date(data.daily[0].dt*1000).toDateString());
-            temp.innerHTML = ("Temp: " + data.daily[0].temp.max) + "&degF / " + (data.daily[0].temp.min) + "&degF";
-             $('#icon').attr('src', "http://openweathermap.org/img/w/" + data.daily[0].weather[0].icon + '.png');
-            console.log('src', "http://openweathermap.org/img/w/" + data.daily[0].weather[0].icon + ".png");
-            description.innerHTML = ("Description: " + data.daily[0].weather[0].description);
-            humidity.innerHTML = ("Humidity: " + data.daily[0].humidity + "%");
-            wind.innerHTML = ("Wind: " + data.daily[0].wind_speed)
-            pressure.innerHTML = ("Pressure: " + data.daily[0].pressure)
+            // date.innerHTML = (new Date(data.daily[0].dt*1000).toDateString());
+            // temp.innerHTML = ("Temp: " + data.daily[0].temp.max) + "&degF / " + (data.daily[0].temp.min) + "&degF";
+            //  $('#icon').attr('src', "http://openweathermap.org/img/w/" + data.daily[0].weather[0].icon + '.png');
+            // description.innerHTML = ("Description: " + data.daily[0].weather[0].description);
+            // humidity.innerHTML = ("Humidity: " + data.daily[0].humidity + "%");
+            // wind.innerHTML = ("Wind: " + data.daily[0].wind_speed)
+            // pressure.innerHTML = ("Pressure: " + data.daily[0].pressure)
+
+            for(var i = 0; i < 5; i++) {
+                var location = data.daily[i].weather[0].icon;
+                var icon = '<img src="http://openweathermap.org/img/w/' + location + '.png" alt="weather icon"/>'
+                var card = '<div class="row">' +
+                    '<div class="card text-center" style="width: 15rem;">' +
+                        '<div id="card">' +
+                        '<br>' +
+                            '<h5>'+ (new Date(data.daily[i].dt*1000).toDateString()) + '</h5>' +
+                            '<p>' + "Temp: " + (data.daily[i].temp.max) + "&degF / " + (data.daily[i].temp.min) + "&degF" +'</p>' +
+                            '<img alt="">'+icon +
+                            '<p>'+ "Description: " + data.daily[i].weather[0].description+'</p>' +
+                            '<p>'+ "Humidity: " + data.daily[i].humidity + "%" + '</p>' +
+                            '<p>'+ "Wind: " + data.daily[i].wind_speed + '</p>' +
+                            '<p>'+ "Pressure: " + data.daily[i].pressure + '</p>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>'
 
 
-
-
+                //console.log(card);
+                $('#container').append(card);
+            }
 
         });
 
