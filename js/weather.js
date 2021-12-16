a4b265807399127adbd2903c03d99bbe20873db9
Jose Diaz <Diaz.M.Jose68@gmail.com>
2021-12-16 02:22:43 -0600

    fixed issue with map not flying to location. now new marker also pops up and is draggable.

--- js/weather.js
+++ js/weather.js
@@ -1,11 +1,14 @@
 'use strict';
 
-var lat = 29.5312;
-var lon = -98.4683;
-var search = $('#userInput').value;
+
 
 $(document).ready(function() {
-    //Get the forecast for 5 days
+    var lat;
+    var lon;
+    //Get the forecast for 5 days using coordinates of variables lon and lat
+    let getWeather = () => {
+        lat = 29.5312;
+        lon = -98.4683;
         $.get("http://api.openweathermap.org/data/2.5/onecall", {
             APPID: OPEN_WEATHER_KEY,
             lat: lat,
@@ -14,9 +17,10 @@ $(document).ready(function() {
         }).done(function (data) {
 
             for (var i = 0; i < 5; i++) {
+                var card = "";
                 var location = data.daily[i].weather[0].icon;
                 var icon = '<img src="http://openweathermap.org/img/w/' + location + '.png" alt="weather icon"/>'
-                var card = '<div class="row">' +
+                card = '<div class="row">' +
                     '<div class="card text-center" style="width: 15rem;">' +
                     '<div id="card">' +
                     '<br>' +
@@ -34,46 +38,73 @@ $(document).ready(function() {
                 $('#container').append(card);
             }
         });
+        console.log('call function weather')
+    }
+    getWeather();
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // Map of central location San Antonio
-    mapboxgl.accessToken = MAPBOX_KEY
-    const map = new mapboxgl.Map({
-        container: 'map', // container ID
-        style: 'mapbox://styles/mapbox/dark-v10', // style URL
-        center: [lon, lat], // starting position [lng, lat]
-        zoom: 10 // starting zoom
-    })
-    // Add zoom and rotation controls to the map.
-    map.addControl(new mapboxgl.NavigationControl());
+    // Map of central location  being at San Antonio
+        mapboxgl.accessToken = MAPBOX_KEY
+        const map = new mapboxgl.Map({
+            container: 'map', // container ID
+            style: 'mapbox://styles/mapbox/dark-v10', // style URL
+            center: [lon, lat], // starting position [lng, lat]
+            zoom: 10 // starting zoom
+        })
+        // Add zoom and rotation controls to the map.
+        map.addControl(new mapboxgl.NavigationControl());
 
-    //places movable marker on central location
-    var dropPin = new mapboxgl.Marker({
-        color: 'green'
-    }).setLngLat([lon, lat]).setDraggable(true);
-    dropPin.addTo(map);
+        //places movable marker on central location
+        var setMarker = new mapboxgl.Marker({
+            color: 'green'
+        }).setLngLat([lon, lat]).setDraggable(true).addTo(map);
 
-    dropPin.on('dragend', function () {
-        dropPin.getLngLat();
-    });
-    console.log(dropPin)
+        setMarker.on('dragend', function () {
+            console.log(setMarker.getLngLat())
+            setMarker.getLngLat();
+        });
+        console.log(setMarker)
 
+
+
+    //when button is clicked, moved map to new location and places a new pin, but can't get it to update weather
+    var search;
+    var lon2;
+    var lat2;
     const button = document.querySelector('.btn');
     button.addEventListener('click', (e) => {
+        search = $('#userInput').val();
         e.preventDefault();
-        geocode($('#userInput').val(), MAPBOX_KEY).then(function (results) {
-            lon = results[0];
-            lat = results[1];
+        geocode(search, MAPBOX_KEY).then(function (results) {
+            console.log(results);
+           lon2 = results[0];
+           lat2 = results[1];
+            console.log(lon2) //console logs to make sure correct coordinates were coming through when searching
+            console.log(lat2)
+        }).then(function (){
+            var newMarker = new mapboxgl.Marker({
+                color: 'blue',
+            }).setLngLat([lon2, lat2]).setDraggable(true).addTo(map);
+
+            newMarker.on('dragend', function () {
+                console.log(newMarker.getLngLat()) //console log to verify coords were changing with movement of drag marker
+                newMarker.getLngLat();
+            });
 
             map.flyTo({
-                center: [lon, lat],
-                zoom: 10,
+                center: [lon2, lat2],
                 essential: true // this animation is considered essential with respect to prefers-reduced-motion
             });
-            console.log("The term searched for was " + search);
+        });
+        //console log to verify that the value of input was being read
+        console.log("The term searched for was " + search);
+
         });
 
 
-    });
 
+
+
+
+//end of document ready function
 });
 
